67ed6355049e2f6cfcf0d8613e5132c7ed51eaff
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-16 00:14:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,20 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   
 end
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  supplies_hash.map do |season, holidays|
-    holidays.map do |holiday, supplies|
-  
-  holiday_hash.collect do |season, holidays|
-    holidays.collect do |keys, values|
-    if values.include?("BBQ")
-      return keys.flatten
-    end
-  end
 
-end
 
 
 

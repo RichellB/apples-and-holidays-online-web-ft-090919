9628517ab3ffafb74953a0bc1ca5b241febcc30a
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-16 00:14:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
     holidays.collect do |keys, values|
     if values.include?("BBQ")
-      return keys.flatten
+      return keys
     end
   end
 
